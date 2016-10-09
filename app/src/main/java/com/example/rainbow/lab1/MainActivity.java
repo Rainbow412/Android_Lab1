fc89c8a699b8cb5cfca973cffaf66ff70a928554
Rainbow Liang <Rainbow Liang>
2016-10-09 21:14:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rainbow/lab1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.rainbow.lab1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 123 on 2016/9/25.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
